d67ff10b6b69b1937d602e20e4651b4985eade93
Vadim <vadimsorella@gmail.com>
2023-04-12 19:13:09 +0600

    Initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+
+    # render text: 'Ваш запрос обработан'
+    # redirect_to '/'
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+end
