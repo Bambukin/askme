018a68ca56c4f384459752d60841b16f5699ad3b
Vadim <vadimsorella@gmail.com>
2023-04-12 22:41:58 +0600

    add views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,14 @@
 class QuestionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
+
   def create
     Question.create(
       body: params[:question][:body],
