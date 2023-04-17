b270882270af42f4f62a2111599f8a7f185072c0
Vadim <vadimsorella@gmail.com>
2023-04-17 19:38:55 +0600

    refactor resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :questions do
+    collection do
+      put :hide
+    end
+  end
+
+  # put '/questions/:id/hide', to: 'questions#hide'
   root to: 'questions#index'
-  resources :questions
-  put '/questions/:id/hide', to: 'questions#hide'
 end
