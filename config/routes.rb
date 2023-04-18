7562985c8b84bad1b0c3a1e247cde81292c1f22e
Vadim <vadimsorella@gmail.com>
2023-04-18 13:40:12 +0600

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
-
   resources :questions do
     member do
       put :hide
     end
   end
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
 
-  # put '/questions/:id/hide', to: 'questions#hide'
   root to: 'questions#index'
 end
