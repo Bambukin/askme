d51adf43a81ae8f27cdb98ffd4f5da4d7979feea
Vadim <vadimsorella@gmail.com>
2023-04-19 23:31:55 +0600

    refactor model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
   has_secure_password
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  private
 
   def downcase_nickname
     nickname.downcase!
