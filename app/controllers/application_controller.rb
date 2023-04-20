ed0db06b932c05e425fcda4d638452370eacf961
Vadim <vadimsorella@gmail.com>
2023-04-20 17:04:43 +0600

    add choice of interface color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :interface_color
 
   private
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def interface_color
+    if session[:user_id]
+      current_user.navbar_color
+    else
+      '#370617'
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_090042) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_095631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_090042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest", null: false
+    t.string "navbar_color", default: "#370617", null: false
   end
 
 end
