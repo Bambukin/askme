1fcfb25867261def1a94f4d1f4b2a9e20e4a28c4
Vadim <vadimsorella@gmail.com>
2023-04-21 14:58:47 +0600

    refactor model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ApplicationRecord
   DEFAULT_NAVBAR_COLOR = '#370617'
-  has_secure_password
-  validates :email, presence: true, uniqueness: true, email: true
-  validates :nickname,
-            presence: true,
-            uniqueness: true,
-            length: { maximum: 40 },
-            format: { with: /\A\w+\z/, message: "only allows latin letters, digits, and underscore" }
-
-  before_save :downcase_nickname
+  before_validation :downcase_nickname
 
   private
 
   def downcase_nickname
     nickname.downcase!
   end
+
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :nickname,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 40 },
+            format: { with: /\A\w+\z/ }
 end
