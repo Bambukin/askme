b324677854fefb170bffdbac9ab6e8705a3ef5d5
Vadim <vadimsorella@gmail.com>
2023-04-25 02:35:58 +0600

    add you in author

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,6 @@
 module QuestionsHelper
   def person_who_asked(question)
+    return 'вы' if question.author == current_user
     if question.author.present?
       link_to "@#{question.author.nickname}", user_path(question.author)
     else
