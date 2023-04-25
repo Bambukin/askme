655fdf545f2fa7403de7cbfab7ddd0b70bac71d4
Vadim <vadimsorella@gmail.com>
2023-04-26 01:56:54 +0600

    nickname in url

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
   resource :session, only: %i[new create destroy]
-  resources :users, except: :index
+  resources :users, except: :index, param: :nickname
 
   root to: 'questions#index'
 end
