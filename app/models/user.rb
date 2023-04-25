03c42ff5158e4d4e881501e25e1807dce9a66e50
Vadim <vadimsorella@gmail.com>
2023-04-26 00:11:15 +0600

    Gravater avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@ class User < ApplicationRecord
   has_many :questions, dependent: :delete_all
   has_many :authored_questions, class_name: 'Question', foreign_key: :author_id, dependent: :nullify
 
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'mp')
+
   private
 
   def downcase_nickname
