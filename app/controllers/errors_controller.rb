892c472ed647e2a2483f003898c13f2c04806edf
Vadim <vadimsorella@gmail.com>
2023-05-24 18:43:06 +0600

    add layout errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,17 @@
 class ErrorsController < ApplicationController
   def not_authorized
-    render status: 403, layout: :errors
+    render status: 403, layout: 'errors'
   end
 
   def not_found
-    render status: 404, layout: :errors
+    render status: 404, layout: 'errors'
   end
 
   def unprocessable_entity
-    render status: 422, layout: :errors
+    render status: 422, layout: 'errors'
   end
 
   def internal_server_error
-    render status: 500, layout: :errors
+    render status: 500, layout: 'errors'
   end
 end
