80501253a6e8c137f369310c2411c525e5e9886f
Vadim <vadimsorella@gmail.com>
2023-05-25 20:44:28 +0600

    add *path

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,21 @@
 class ErrorsController < ApplicationController
   def not_authorized
+    @title = '403'
     render status: 403, layout: 'errors'
   end
 
   def not_found
+    @title = '404'
     render status: 404, layout: 'errors'
   end
 
   def unprocessable_entity
+    @title = '422'
     render status: 422, layout: 'errors'
   end
 
   def internal_server_error
+    @title = '500'
     render status: 500, layout: 'errors'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   match '/404' => 'errors#not_found',             via: :all, as: :not_found
   match '/422' => 'errors#unprocessable_entity',  via: :all, as: :unprocessable_entity
   match '/500' => 'errors#internal_server_error', via: :all, as: :internal_server_error
+  match '*path' => 'errors#not_found',            via: :all
 end
